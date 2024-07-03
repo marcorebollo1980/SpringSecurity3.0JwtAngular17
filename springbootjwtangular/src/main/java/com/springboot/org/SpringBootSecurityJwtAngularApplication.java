fadeb9786db646ce379d53e12ac7b51d99d396f4
Marco Antonio Rebollo Lopez <marcorebollolopez@gmail.com>
2024-07-03 16:58:06 -0500

    commit firts

---
+++ springbootjwtangular/src/main/java/com/springboot/org/SpringBootSecurityJwtAngularApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJwtAngularApplication.class, args);
+	}
+
+}
