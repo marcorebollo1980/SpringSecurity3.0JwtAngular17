e5a52ca9f37af82936cd3d0ed8d6b80bb2817554
Marco Antonio Rebollo Lopez <marcorebollolopez@gmail.com>
2024-07-05 09:50:32 -0500

    commit

---
+++ demo/src/test/java/com/springboot/jwt/org/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jwt.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
